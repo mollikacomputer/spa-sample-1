5ee48cbc7b649bfa9e293c4371e24a0d86da06d5
mollikacomputer <mollikacomputer3@gmail.com>
2023-01-15 08:52:57 +0600

    email field update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,91 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../firebase.init";
+import Loading from "../shared/Loading";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  if (user) {
+    console.log(user.user.displayName);
+  }
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl ">
         <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <h2 className="text-2xl font-bold">Log In</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">What is your name?</span>
+            </label>
+            <input 
+            type="email" 
+            placeholder="Type Email" 
+            class="input input-bordered w-full max-w-xs" 
+            {
+              ...register(
+              "email", 
+            {required: true },
+            {
+              pattern: {
+                value: /[A-Za-z]{3}/,
+                message: 'Please provide a valid email'
+              }
+            }
+            )
+          }
+            aria-invalid={errors.email ? "true" : "false"}
+            />
+            <label class="label">
+              <span class="label-text-alt">
+                {errors.email?.type === "required" && (<p role="alert">Email name is required</p>)}
+                {errors.email?.type === "pattern" && (<p role="alert" className="text-red-500"> {errors.email?.message} </p>)}
+                </span>
+              <span class="label-text-alt">Alt label</span>
+            </label>
           </div>
+
+
+
+
+
+            <input
+              
+              
+            />
+            
+
+            <input
+              {...register("mail", { required: "Email Address is required" })}
+              aria-invalid={errors.mail ? "true" : "false"}
+            />
+            {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+
+            <input className="btn btn-block" type="submit" />
+          </form>
         </div>
         <div class="divider">OR</div>
-        <button class="btn btn-outline">Login With Google</button>
+        <button class="btn btn-outline" onClick={() => signInWithGoogle()}>
+          Login With Google
+        </button>
       </div>
-      
     </div>
   );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDJXoxT1A2k8sSulUuvr9Hs51XqESlarA",
-  authDomain: "spa-sample-1.firebaseapp.com",
-  projectId: "spa-sample-1",
-  storageBucket: "spa-sample-1.appspot.com",
-  messagingSenderId: "595884805902",
-  appId: "1:595884805902:web:60f1e27d8d2725080c915f"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
